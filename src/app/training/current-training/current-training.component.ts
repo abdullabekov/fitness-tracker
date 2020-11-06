ef0837d345344c797f4f9a440cf2ae8028da8651
Denis Abdullabekov <mrden@inbox.ru>
2020-11-06 21:29:42 +1000

    55 - training

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { StopTrainingComponent } from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
+  timer: number;
+  @Output() trainingExit = new EventEmitter();
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.startOrResumeTimer();
   }
 
+  startOrResumeTimer(): void {
+    this.timer = setInterval(() => {
+      if (this.progress >= 100) {
+        this.stopTimer();
+      } else {
+        this.progress += 5;
+      }
+    }, 1000);
+  }
+
+  onStop(): void {
+    clearInterval(this.timer);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+    }});
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
+    });
+  }
+
+  private stopTimer(): void {
+    clearInterval(this.timer);
+  }
 }
